365be78424418ef6b620bd6a2511cdda767e84df
¨fatima <fpersidanniaux@uade.edu.ar>
2025-06-01 19:12:18 -0300

    modularizo la carga de turno

---
+++ sistema_turnos/datos.py
@@ -0,0 +1,10 @@
+def cargar_turnos():
+    """
+    Devuelve una lista con los turnos iniciales disponibles,
+    incluyendo fecha, hora, profesional y servicio.
+    """
+    return [
+        {"fecha_hora": ("2025-04-26", "10:00"), "profesional": "Gisela", "servicio": "Kapping"},
+        {"fecha_hora": ("2025-04-25", "14:00"), "profesional": "Marisol", "servicio": "Semi"},
+        {"fecha_hora": ("2025-04-26", "16:00"), "profesional": "Valentina", "servicio": "Soft Gel"},
+    ]
